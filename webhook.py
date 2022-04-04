4578643bebf7168e0378755c0a49a81d500b3ef7
Chris Weber <chrisweber@C02G51SKMD6R.lan>
2022-04-04 09:54:57 -0400

    wh

--- webhook.py
+++ webhook.py
@@ -1,11 +1,12 @@
 import requests
 import json
 
-webhook_url="https://fh-demo.com/webhook"
+webhook_url="https://b8a2a272a2a6.ngrok.io/webhook"
+webhook_url2="https://fh-demo.com/webhook"
 
 data = {
     "name":"chris"
 }
 
-r = requests.post(webhook_url, data=json.dumps(data), headers={'Content-type':'application/json'})
+r = requests.post(webhook_url2, data=json.dumps(data), headers={'Content-type':'application/json'})
 
